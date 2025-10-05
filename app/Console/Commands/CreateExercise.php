a2bd565be22faa6c04e42b9db55ffc625f76bf5d
Andrew Cauchi <drewcauchi@gmail.com>
2025-10-05 14:16:18 +0200

    fix: ambiguous createexercise name

--- app/Console/Commands/CreateExercise.php
+++ app/Console/Commands/CreateExercise.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Actions\Question\CreateQuestion;
+use App\Actions\Exercise\CreateExercise as CreateExerciseAction;
 use App\Services\Exercise\ExerciseService;
 use Exception;
 use Illuminate\Console\Command;
@@ -20,18 +21,18 @@ class CreateExercise extends Command
      */
     protected $description = 'Command description';
 
-    public function handle(ExerciseService $exerciseService, CreateExercise $createExercise, CreateQuestion $createQuestion): void
+    public function handle(ExerciseService $exerciseService, CreateExerciseAction $createExerciseAction, CreateQuestion $createQuestion): void
     {
         try {
             $response = $exerciseService->get();
             $data = $exerciseService->process($response);
 
-            DB::transaction(function () use ($exerciseService, $createExercise, $createQuestion, $data) {
+            DB::transaction(function () use ($exerciseService, $createExerciseAction, $createQuestion, $data) {
                 $validatedExercise = $exerciseService->validateExercise();
-                $exercise = $createExercise->handle($validatedExercise);
+                $exercise = $createExerciseAction->handle($validatedExercise);
 
                 foreach ($data as $question) {
-                    $validatedQuestion = $exerciseService->validateQuestion($data, $exercise->id);
+                    $validatedQuestion = $exerciseService->validateQuestion($question, $exercise->id);
                     $createQuestion->handle($validatedQuestion);
                 }
             });

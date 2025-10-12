5b5b1f71729cdb9df80ea21f39858cb9c3e0ec8b
Andrew Cauchi <drewcauchi@gmail.com>
2025-10-12 19:46:05 +0200

    feat: SSR implemented + schedule

--- resources/js/Pages/Exercises/GapAttack/Show.vue
+++ resources/js/Pages/Exercises/GapAttack/Show.vue
@@ -8,21 +8,25 @@ const props = defineProps({
 </script>
 
 <template>
-    <Head :title="props.exercise?.title ?? 'Daily Game'" />
-        <PageTitle
-            v-if="props.exercise?.title !== '' && props.exercise?.description !== ''"
-            :title="props.exercise.title"
-            :description="props.exercise.description"
-        />
+    <Head :title="props.exercise?.title || 'Daily Game'" />
 
-        <div class="grid w-full max-w-md m-auto" v-if="props.exercise?.questions?.length > 0">
-            <FillInTheBlanks
-                v-for="(question, index) in props.exercise?.questions"
-                :key="index"
-                :question="question"
-                :z-index="props.exercise?.questions.length - index"
-                :index="index"
-                class="col-start-1 row-start-1"
-            />
-        </div>
+    <PageTitle
+        v-if="props.exercise?.title || props.exercise?.description"
+        :title="props.exercise?.title || 'Daily Game'"
+        :description="props.exercise?.description || 'There are no daily games at the moment'"
+    />
+
+    <div
+        class="grid w-full max-w-md m-auto"
+        v-if="props.exercise?.questions?.length"
+    >
+        <FillInTheBlanks
+            v-for="(question, index) in props.exercise.questions"
+            :key="index"
+            :question="question"
+            :z-index="props.exercise.questions.length - index"
+            :index="index"
+            class="col-start-1 row-start-1"
+        />
+    </div>
 </template>
---
+++ resources/js/ssr.js
@@ -0,0 +1,37 @@
+import { createInertiaApp, Head, Link } from '@inertiajs/vue3'
+import createServer from '@inertiajs/vue3/server'
+import { renderToString } from '@vue/server-renderer'
+import { createSSRApp, h } from 'vue'
+import { ZiggyVue } from "ziggy-js";
+import MainLayout from "@/Layouts/MainLayout.vue";
+import CustomButton from "@/Components/Buttons/CustomButton.vue";
+
+createServer(page =>
+    createInertiaApp({
+        page,
+        render: renderToString,
+        title: (title) => `Leerdle | ${title}`,
+        resolve: (name) => {
+            const pages = import.meta.glob('./Pages/**/*.vue', {
+                eager: true
+            });
+            let page = pages[`./Pages/${name}.vue`];
+
+            page.default.layout = page.default.layout || MainLayout;
+            return page;
+        },
+        setup({ App, props, plugin }) {
+            return createSSRApp({
+                render: () => h(App, props),
+            }).use(plugin)
+                .use(ZiggyVue, {
+                    ...page.props.ziggy,
+                    location: new URL(page.props.ziggy.location),
+                })
+                .component("Head", Head)
+                .component("Link", Link)
+                .component("Button", CustomButton)
+        },
+    }),
+    { cluster: true },
+)
